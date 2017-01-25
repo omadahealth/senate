855a0911c20ac2be9b8991412e1beb9939eca570
Kevin G. Phillips <kevin.phillips@omadahealth.com>
2017-01-25 11:21:09 -0800

    [rawhide-rspec2] Configure factory_girl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,6 +68,8 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
+  Dir[Rails.root.join("spec/**/support/**/*.rb")].each { |f| require f }
+
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
